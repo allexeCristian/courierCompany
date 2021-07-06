7d1af2ed3337079f3925b6adc498d3ce37e3aac5
allexeCristian <allexe.cristian@gmail.com>
2021-07-06 15:07:26 +0300

    Add first gui form

--- src/com/courierCompany/App.java
+++ src/com/courierCompany/App.java
@@ -1,4 +1,16 @@
 package com.courierCompany;
 
+import javax.swing.*;
+
 public class App {
+    private JRadioButton radioButton1;
+    private JPanel panelPrincipal;
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panelPrincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
